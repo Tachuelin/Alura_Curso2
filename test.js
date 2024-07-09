351d195ed94e98a4d355d81490c9c613190a08ba
Tachuelin <darellanoa1300@alumno.ipn.mx>
2024-07-09 14:41:32 -0600

    version_final_curso_funciones

---
+++ test.js
@@ -0,0 +1,19 @@
+function encontrarMayorMenor(lista) {
+    let mayor = lista[0];
+    let menor = lista[0];
+  
+    for (let i = 1; i < lista.length; i++) {
+      if (lista[i] > mayor) {
+        mayor = lista[i];
+      }
+      if (lista[i] < menor) {
+        menor = lista[i];
+      }
+    }
+  
+    console.log('Mayor:', mayor);
+    console.log('Menor:', menor);
+  }
+  
+  let numeros = [15, 8, 25, 5, 12];
+  encontrarMayorMenor(numeros);
